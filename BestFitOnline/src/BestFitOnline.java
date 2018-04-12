a67ee10ec17cf11bb4bc26ca063c0e235e17a2ac
ddt-ddt <271162889@qq.com>
2018-04-12 14:13:01 +0800

    Update BestFitOnline.java

--- BestFitOnline/src/BestFitOnline.java
+++ BestFitOnline/src/BestFitOnline.java
@@ -4,36 +4,36 @@ import java.util.Vector;
 
 public class BestFitOnline{
 
-	static //VM×ÊÔ´Õ¼ÓÃÇé¿ö
+	static //VMèµ„æºå ç”¨æƒ…å†µ
 	double VM[][] = {{6.25,5.21,5.25},{12.5,10.42,10.5},{6.25,1.18,2.19},{25,4.86,5.25},{100,42.1,21},{50,15.63,10.5},{12.5,23.75,5.31},{25,47.5,10.5}};	
 	public static void MaxUtil(int JobNumber){
 		int ServerNumber=1000;
 		int LastTime=0;
 		int CMU_P=0;
 		
-		Job job[]=new Job[JobNumber+1];//³õÊ¼»¯×÷Òµ
+		Job job[]=new Job[JobNumber+1];//åˆå§‹åŒ–ä½œä¸š
 		double P=0.0;
 		int ServerNumberNow=0;
 		for(int i=1;i<=JobNumber;i++){
 			job[i]=new Job(i);
 		}
-		HashMap<Integer,Vector> ServerJob =new HashMap<Integer, Vector>();//ÓÃÓÚ´æ´¢·şÎñÆ÷×÷Òµkey-value
+		HashMap<Integer,Vector> ServerJob =new HashMap<Integer, Vector>();//ç”¨äºå­˜å‚¨æœåŠ¡å™¨ä½œä¸škey-value
 		for(int i=0;i<ServerNumber;i++)
 		{
 			Vector v = new Vector();
 			ServerJob.put(i,v);
 		}
 		
-		double[] UtilizationServer=new double[ServerNumber];//Ã¿¸ö·şÎñÆ÷µÄCPU×ÊÔ´ÀûÓÃÂÊ
-		double[][] ActualUtilizationServer = new double[ServerNumber][3];//Ã¿¸ö·şÎñÆ÷µÄ×ÊÔ´ÀûÓÃÂÊ
+		double[] UtilizationServer=new double[ServerNumber];//æ¯ä¸ªæœåŠ¡å™¨çš„CPUèµ„æºåˆ©ç”¨ç‡
+		double[][] ActualUtilizationServer = new double[ServerNumber][3];//æ¯ä¸ªæœåŠ¡å™¨çš„èµ„æºåˆ©ç”¨ç‡
 		for(int i=0;i<ServerNumber;i++){
 			ActualUtilizationServer[i][0]=100;
 			ActualUtilizationServer[i][1]=100;
 			ActualUtilizationServer[i][2]=100;
 		}
 		
-		int[] ServerStartTime=new int[ServerNumber];//Ã¿¸ö·şÎñÆ÷µÄ¿ªÊ¼Ê±¼ä
-		int[] ServerFinishTime=new int[ServerNumber];//¼ÇÂ¼·şÎñÆ÷µÄ½áÊøÊ±¿Ì
+		int[] ServerStartTime=new int[ServerNumber];//æ¯ä¸ªæœåŠ¡å™¨çš„å¼€å§‹æ—¶é—´
+		int[] ServerFinishTime=new int[ServerNumber];//è®°å½•æœåŠ¡å™¨çš„ç»“æŸæ—¶åˆ»
 		
 		for(int i=0;i<ServerNumber;i++){
 			ServerFinishTime[i]=0;
@@ -47,24 +47,16 @@ public class BestFitOnline{
 		Vector Done_Job = new Vector();
 		
 		for(int i=1;i<=JobNumber;i++){
-System.out.println();
-System.out.println("Job::"+i);
+			System.out.println();
+			System.out.println("Job::"+i);
 			OnDo_Job.add(i);
-//System.out.println("OnDo_Job:"+OnDo_Job);
 			int T_now=job[i].getArrive_Time();
-System.out.println("T_now:"+T_now);
-			int vn=job[i].getVn();//ĞéÄâ»úµÄ×îĞ¡ÊıÄ¿		
-			
+			System.out.println("T_now:"+T_now);
+			int vn=job[i].getVn();//è™šæ‹Ÿæœºçš„æœ€å°æ•°ç›®					
 			for(int j=0;j<OnDo_Job.size();j++){
 				int temp=(int) OnDo_Job.get(j);
-//System.out.println("j::"+j);
-//System.out.println("JobNumber:"+temp);
-//System.out.println("T_now:"+T_now);
 				if(job[temp].getFinish_Time()<=T_now){
 					Done_Job.add(temp);
-//System.out.println("Done_Job:"+Done_Job);
-					
-//System.out.println("OnDo_Job222222222:"+OnDo_Job);
 				}
 			}
 			while(Done_Job.size()>0){
@@ -80,40 +72,24 @@ System.out.println("T_now:"+T_now);
 				OnDo_Job.removeElement(min_Job);
 				
 				if(min_Job!=0){
-					if(job[min_Job].getFinish_Time()<=T_now){//±íÊ¾ÓĞ×÷ÒµÍê³É
-//System.out.println();
-System.out.println("FinishTime::"+job[min_Job].getFinish_Time());
+					if(job[min_Job].getFinish_Time()<=T_now){//è¡¨ç¤ºæœ‰ä½œä¸šå®Œæˆ
+						System.out.println("FinishTime::"+job[min_Job].getFinish_Time());
 						Done_Job.removeElement(min_Job);
-//System.out.println("Done_Job333333333:"+Done_Job);
-//System.out.println("OnDo_Job333333333:"+OnDo_Job);
 						for(int z=0;z<ServerNumber;z++){
-//System.out.println(ServerJob);
-//System.out.println(z);
 							if(ServerJob.get(z).contains(min_Job)){
 								P= (0.8622 * 2.66*2.66*2.66 *UtilizationServer[z]+ 18.905)*32;
 								Energy+=(job[min_Job].getFinish_Time()-ServerStartTime[z])*P;
 								CMU+=(job[min_Job].getFinish_Time()-ServerStartTime[z]);
-	
 								ServerStartTime[z]=job[min_Job].getFinish_Time();
-	
-//System.out.println("33333::::::"+ServerFinishTime[z]+";;;;"+ServerStartTime[z]);
 							}
-							while(ServerJob.get(z).contains(min_Job)){//ÅĞ¶Ï¸Ã×÷ÒµÊÇ·ñ°üº¬jÕâ¸ö×÷Òµ
-		//System.out.println("@@@@11111ServerJob:"+ServerJob);					
-													
+							while(ServerJob.get(z).contains(min_Job)){//åˆ¤æ–­è¯¥ä½œä¸šæ˜¯å¦åŒ…å«jè¿™ä¸ªä½œä¸š													
 								UtilizationServer[z]-=job[min_Job].getUtilization();
 								ActualUtilizationServer[z][0]+=VM[job[min_Job].getVMtype()-1][0];
 								ActualUtilizationServer[z][1]+=VM[job[min_Job].getVMtype()-1][1];
 								ActualUtilizationServer[z][2]+=VM[job[min_Job].getVMtype()-1][2];
-								ServerJob.get(z).removeElement(min_Job);
-		//System.out.println("@@@@22222ServerJob:"+ServerJob);											
-													
-								if(ActualUtilizationServer[z][0]==100&&ActualUtilizationServer[z][1]==100&&ActualUtilizationServer[z][2]==100){//¸Ã·şÎñÆ÷ÉÏµÄ×÷ÒµÈ«²¿Íê³É
-	
+								ServerJob.get(z).removeElement(min_Job);					
+								if(ActualUtilizationServer[z][0]==100&&ActualUtilizationServer[z][1]==100&&ActualUtilizationServer[z][2]==100){//è¯¥æœåŠ¡å™¨ä¸Šçš„ä½œä¸šå…¨éƒ¨å®Œæˆ
 									ServerNumberNow--;
-//System.out.println("ServerNumberNow::"+ServerNumberNow);
-//System.out.println(ServerFinishTime[z]+";;;;"+ServerStartTime[z]);
-	
 									ServerStartTime[z]=0;
 									ServerFinishTime[z]=0;
 									ServerJob.get(z).clear();
@@ -124,12 +100,11 @@ System.out.println("FinishTime::"+job[min_Job].getFinish_Time());
 				}
 				P= (0.8622 * 2.66*2.66*2.66 + 18.905)*32;
 				E_MAX=CMU*P;
-System.out.println("E::"+Energy);
-System.out.println("ServerNumberNow::"+ServerNumberNow);
-System.out.println("CMU*P_MAX::"+E_MAX);
-System.out.println("CMU_P:"+(E_MAX-Energy)/1000);
-System.out.println();
-					
+				System.out.println("E::"+Energy);
+				System.out.println("ServerNumberNow::"+ServerNumberNow);
+				System.out.println("CMU*P_MAX::"+E_MAX);
+				System.out.println("CMU_P:"+(E_MAX-Energy)/1000);
+				System.out.println();				
 			}
 			
 			int[] flag=new int[ServerNumber];
@@ -137,26 +112,24 @@ System.out.println();
 					flag[p]=0;
 				}
 			while(vn>0){
-				double[][] f=new double[JobNumber+1][ServerNumber];//ÅĞ¶Ïº¯Êı
+				double[][] f=new double[JobNumber+1][ServerNumber];//åˆ¤æ–­å‡½æ•°
 				int maxServer=0;
 				double max=0.0;
 				
 				for(int y=0;y<ServerNumber;y++){
-					if(ActualUtilizationServer[y][0]<100||ActualUtilizationServer[y][1]<100||ActualUtilizationServer[y][2]<100){//±éÀúÒÑÊ¹ÓÃµÄ·şÎñÆ÷
+					if(ActualUtilizationServer[y][0]<100||ActualUtilizationServer[y][1]<100||ActualUtilizationServer[y][2]<100){//éå†å·²ä½¿ç”¨çš„æœåŠ¡å™¨
 						if(ActualUtilizationServer[y][0]>=VM[job[i].getVMtype()-1][0]&&ActualUtilizationServer[y][1]>=VM[job[i].getVMtype()-1][1]&&ActualUtilizationServer[y][2]>=VM[job[i].getVMtype()-1][2]){
-							f[i][y]=UtilizationServer[y]+job[i].getUtilization();		
-							
+							f[i][y]=UtilizationServer[y]+job[i].getUtilization();									
 						}
 						else
 							continue;
 					}
-					else if(ActualUtilizationServer[y][0]==100&&ActualUtilizationServer[y][1]==100&&ActualUtilizationServer[y][2]==100){//È«ĞÂµÄ·şÎñÆ÷
+					else if(ActualUtilizationServer[y][0]==100&&ActualUtilizationServer[y][1]==100&&ActualUtilizationServer[y][2]==100){//å…¨æ–°çš„æœåŠ¡å™¨
 					
 						f[i][y]=job[i].getUtilization();
 					}
 					else if(ActualUtilizationServer[y][0]<=0||ActualUtilizationServer[y][1]<=0||ActualUtilizationServer[y][2]<=0)
-					{	
-					
+					{				
 						break;
 					}
 					if(f[i][y]>max){
@@ -180,12 +153,10 @@ System.out.println();
 						CMU_P+=(job[i].getArrive_Time()-LastTime)*ServerNumberNow*(0.8622 * 2.66*2.66*2.66 + 18.905)*32;
 						LastTime=job[i].getArrive_Time();
 						ServerStartTime[maxServer]=job[i].getArrive_Time();
-//System.out.println("4444444:::::"+ServerFinishTime[maxServer]+";;;;"+ServerStartTime[maxServer]);
 					}
 
 					if(job[i].getFinish_Time()>ServerFinishTime[maxServer]){
 						ServerFinishTime[maxServer]=job[i].getFinish_Time();
-
 					}
 				}
 				UtilizationServer[maxServer]+=job[i].getUtilization();
@@ -194,19 +165,18 @@ System.out.println();
 				ActualUtilizationServer[maxServer][1]-=VM[job[i].getVMtype()-1][1];
 				ActualUtilizationServer[maxServer][2]-=VM[job[i].getVMtype()-1][2];
 
-				ServerJob.get(maxServer).add(i);//½«×÷Òµi¼ÓÈëµ½ServerJobÖĞ
+				ServerJob.get(maxServer).add(i);//å°†ä½œä¸šiåŠ å…¥åˆ°ServerJobä¸­
 				vn--;
 			}
 			P= (0.8622 * 2.66*2.66*2.66 + 18.905)*32;
 			E_MAX=CMU*P;
-System.out.println("E::"+Energy);
-System.out.println("CMU*P_MAX::"+E_MAX);
-System.out.println("ServerNumberNow::"+ServerNumberNow);
-System.out.println("CMU_P:"+(E_MAX-Energy)/1000);
+			System.out.println("E::"+Energy);
+			System.out.println("CMU*P_MAX::"+E_MAX);
+			System.out.println("ServerNumberNow::"+ServerNumberNow);
+			System.out.println("CMU_P:"+(E_MAX-Energy)/1000);
 		}
 		Energy=Energy/1000;
-System.out.println("bbb"+Energy);
-		
+		System.out.println("bbb"+Energy);
 	}
 	
 	public static void main(String[] args) {
